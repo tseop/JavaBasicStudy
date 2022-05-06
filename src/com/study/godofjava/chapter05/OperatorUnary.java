a98af634116cc7932ad8942d14c85369242c8eea
tseop <77163842+tseop@users.noreply.github.com>
2022-05-06 17:50:35 +0900

    Chapter05 update
    Chapter05 update

---
+++ src/com/study/godofjava/chapter05/OperatorUnary.java
@@ -0,0 +1,16 @@
+package com.study.godofjava.chapter05;
+
+public class OperatorUnary {
+	public static void main(String[] args) {
+		OperatorUnary sample = new OperatorUnary();
+		sample.complement();
+
+	}
+
+	public void complement() {
+		boolean flag = true;
+		System.out.println(flag);
+		System.out.println(!flag);
+
+	}
+}
