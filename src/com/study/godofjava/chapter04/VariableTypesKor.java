90f350902017669448d861ed52bfc30bc8691336
tseop <77163842+tseop@users.noreply.github.com>
2022-05-06 16:22:44 +0900

    Chapter04 close-out
    Chapter04 close-out

---
+++ src/com/study/godofjava/chapter04/VariableTypesKor.java
@@ -0,0 +1,15 @@
+package com.study.godofjava.chapter04;
+
+public class VariableTypesKor {
+
+	int instanceVariable; // 인스턴스 변수
+	static int classVariable; // 클래스 변수
+
+	public static void main(String[] args) {
+		int localVariable; // 지역변수
+	}
+
+	public int add(int num1, int num2) { // 매개변수
+		return num1 + num2;
+	}
+}
