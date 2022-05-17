dfb52380d2c6f891c075962d93545fd04c910c37
tseop <77163842+tseop@users.noreply.github.com>
2022-05-17 10:05:58 +0900

    Chapter09 close-out
    Chapter09 close-out

---
+++ src/com/study/godofjava/chapter09/PackageStatic.java
@@ -0,0 +1,10 @@
+package com.study.godofjava.chapter09;
+
+import static com.study.godofjava.chapter09.SubStatic.*;
+
+public class PackageStatic {
+	public static void main(String[] args) {
+		subStaticMethod();
+		System.out.println(CLASS_NAME);
+	}
+}
